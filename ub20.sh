4ddcb28f9cfddf619cc15097c5e404fa7641c598
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-06-16 19:12:04 +0700

    Update ub20.sh

--- ub20.sh
+++ ub20.sh
@@ -38,7 +38,7 @@ RAMMS=$(free -m | awk 'NR==2 {print $2}')
 KEY="5621227316:AAFp_SxXGwD9NE0HgSBCU0sP4fsJmuo1YzE"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 GITHUB_CMD="https://github.com/sibeesans/apacok/raw/"
-NAMECOM=$(curl -sS https://raw.githubusercontent.com/sibeesans/apacok/main/pelanggan | grep $MYIP | awk '{print $2}')
+NAMECOM=$(curl -sS https://raw.githubusercontent.com/sibeesans/jensek/main/boik | grep $MYIP | awk '{print $2}')
 OS=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 Date_list=$(date +"%Y-%m-%d" -d "$dateFromServer")
@@ -258,7 +258,7 @@ END
     fi
 }
 FIGHTERTUNNEL() {
-    curl -sS https://raw.githubusercontent.com/sibeesans/apacok/main/pelanggan >/root/tmp
+    curl -sS https://raw.githubusercontent.com/sibeesans/jensek/main/boik >/root/tmp
     data=($(cat /root/tmp | grep -E "^### " | awk '{print $2}'))
     for user in "${data[@]}"; do
         exp=($(grep -E "^### $user" "/root/tmp" | awk '{print $3}'))
@@ -301,7 +301,7 @@ function configure_nginx() {
 }
 ftTunneling() {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/sibeesans/apacok/main/pelanggan | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/sibeesans/jensek/main/boik | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
         TIMEDATE
     else
